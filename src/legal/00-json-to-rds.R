6a08d81aa96ed1c1db328e725026d6287c5e7aa5
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2020-07-19 08:12:34 +0200

    new error handling

--- src/legal/00-json-to-rds.R
+++ src/legal/00-json-to-rds.R
@@ -1,4 +1,5 @@
 library(dplyr)
+
 library(jsonlite)
 library(pbmcapply)
 library(rvest)
@@ -7,37 +8,41 @@ library(stringr)
 rm(list=ls())
 
 getwd()
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 #datasets <- list.dirs('../../input/bulk-data', full.names = T)[-1]
-datasets <- list.dirs('/Volumes/INTENSO/legal_tc/input/bulk-data/', full.names = T)
+#datasets <- list.dirs('/Volumes/INTENSO/legal_tc/input/bulk-data/', full.names = T)
+#head(datasets)
 
-for(i in datasets){
+setwd('/Volumes/INTENSO/legal_tc/input/bulk-data/')
+getwd()
+html <- read_html('https://www.courtlistener.com/api/bulk-info/')
+datafiles <- html_node(html, '.table.settings-table') %>% html_table() %>% setNames(., tolower(names(.)))
+datasets <- datafiles$abbreviation
 
-  # get list of all .json-files of dataset
-  files <- list.files(i, full.names = T)
-  # run through .json-files and extract the text
-  df <- pbmclapply(files, function(x){
-    df <- try(fromJSON(x))
-    if('try-error'%in%class(df)){
-      write(x, file="out.txt", append=T)
-      return(NULL)
+transcribe <- function(df){
+  if('try-error'%in%class(df)){
+    write(x, file="out.txt", append=T)
+    return(NULL)
+  }else{
+    id <- df$id
+    path_date <- df$local_path
+    if(ifelse(identical(!df$html=="", logical(0)), F, !df$html=="")){
+      df <- df$html
+    }else if(ifelse(identical(!df$html_with_citations=="", logical(0)), F, !df$html_with_citations=="")){
+      df <- df$html_with_citations
+    }else if(ifelse(identical(!df$html_lawbox=="", logical(0)), F, !df$html_lawbox=="")){
+      df <- df$html_lawbox
+    }else if(ifelse(identical(!df$html_columbia=="", logical(0)), F, !df$html_columbia=="")){
+      df <- df$html_columbia
+    }else if(ifelse(identical(!df$html_harvard=="", logical(0)), F, !df$html_harvard=="")){
+      df <- df$html_harvard
+    }else if(ifelse(identical(!df$plain_text=="", logical(0)), F, !df$plain_text=="")){
+      df <- df$plain_text
+    }
+    if(ifelse(identical(logical(0), grepl('pdf', path_date)), F, grepl('pdf', path_date))){
+      df_date <- gsub('pdf\\/(.*?)\\/.*', '\\1', path_date)
     }else{
-      id <- df$id
-      if(ifelse(identical(!df$html=="", logical(0)), F, !df$html=="")){
-        df <- df$html
-      }else if(ifelse(identical(!df$html_with_citations=="", logical(0)), F, !df$html_with_citations=="")){
-        df <- df$html_with_citations
-      }else if(ifelse(identical(!df$html_lawbox=="", logical(0)), F, !df$html_lawbox=="")){
-        df <- df$html_lawbox
-      }else if(ifelse(identical(!df$html_columbia=="", logical(0)), F, !df$html_columbia=="")){
-        df <- df$html_columbia
-      }else if(ifelse(identical(!df$html_harvard=="", logical(0)), F, !df$html_harvard=="")){
-        df <- df$html_harvard
-      }else if(ifelse(identical(!df$plain_text=="", logical(0)), F, !df$plain_text=="")){
-        df <- df$plain_text
-      }
-
       df_try <- try(read_html(df))
       if(!'try-error'%in%class(df_try)){
         df_date <- try(html_text(html_node(df_try, css='.date')))
@@ -52,9 +57,28 @@ for(i in datasets){
           return(NULL)
         }
       }
-      return(list(id, df_date, df))
     }
-  }, mc.cores = 4)
+    id <- ifelse(is.null(id), NA, id)
+    df_date <- ifelse(is.null(df_date), NA, df_date)
+    df <- ifelse(is.null(df), NA, df)
+    return(list(id, df_date, df))
+  }
+}
+
+for(i in datasets[8]){
+  print(i)
+  if(paste(i, '.RDS') %in% list.files('/Volumes/INTENSO/legal_tc/output/00-bulk-data/legal/')) next
+  #i = datasets[2]
+  # get list of all .json-files of dataset
+  files <- list.files(i, full.names = T)
+  # run through .json-files and extract the text
+  df <- pbmclapply(files, function(x){
+    df <- try(fromJSON(x))
+    df <- tryCatch(transcribe(df=df), error = function(e) return(NULL))
+    return(df)
+  } 
+  , mc.cores = 5
+  )
   # collapse data
   df <- do.call(rbind, df)
   # coerce to tibble
@@ -69,7 +93,9 @@ for(i in datasets){
   df <- filter(df, !lengths(txt)>1)
   # collapse text list to vec
   if(length(unlist(df$txt)) == nrow(df)) df <- mutate(df, txt=unlist(txt))
+  df <- mutate(df, court = i)
 
   out <- paste0('/Volumes/INTENSO/legal_tc/output/00-bulk-data/legal/', gsub('.*\\/', '', i), '.RDS')
   save(df, file = out)
+  write(i, file = '~/legal_tc/src/legal/json-log.txt', append = T)
 }
