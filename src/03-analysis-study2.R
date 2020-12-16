ed77f5a81300c37b9a05fb56bb7cdfb673612c60
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2020-12-16 13:56:18 +0100

    study 2 updated w trash

--- src/03-analysis-study2.R
+++ src/03-analysis-study2.R
@@ -4,6 +4,9 @@ library(ggplot2)
 library(dplyr)
 library(emmeans)
 library(xtable)
+library(factoextra)
+library(Ckmeans.1d.dp)
+
 
 rm(list=ls())
 
@@ -24,3 +27,38 @@ m1 <- lm(abs(sentiWords) ~ cat, data = df)
 emm1 <- emmeans(m1, specs = pairwise ~ cat)
 res1 <- xtable(emm1$contrasts)
 print(res1, include.rownames = F)
+
+# K-Means Cluster Analysis
+aggr <- df %>% 
+  #mutate(group = paste0(TARGET, '_', TARGET_pol)) %>% 
+  #group_by(group) %>% 
+  group_by(cat, TARGET) %>% 
+  summarise(means = mean(abs(sentiWords), na.rm =T),
+            lex = paste0(ADJ, collapse = ' '))
+
+res.mean <- Ckmeans.1d.dp(aggr$means, 8)
+aggr <- cbind(aggr, res.mean = res.mean$cluster)
+
+p <- ggplot(aggr, aes(x=means, y=TARGET)) +
+  geom_text(aes(label = res.mean)) +
+  scale_x_continuous(limits = c(-.5,.5)) +
+  facet_grid(cat~., scales = 'free_y', space = "free_y", drop = T) +
+  labs(
+    y = 'Target Ajective',
+    x = 'Average SentiWords Score of\nConjoined Adjectives',
+    caption = abbrv('NTC: Neg. Thick Concepts; NVAC: Neg. Value-Assciated Concepts; NThin: Neg. Thin Concepts; DC: Descriptive Concepts; PThin: Pos. Thin Concepts; PVAC: Pos. Value-Associated Concepts; PTC: Pos. Thick Concepts.', width = 75)
+  ) 
+p
+
+aggr$lex_div <- textstat_lexdiv(tokens(aggr$lex), measure = 'TTR')$TTR
+aggr <- select(aggr, -lex) %>% as.data.frame()
+row.names(aggr) <- aggr$group
+res.kmeans <- eclust(aggr[, c('means', 'lex_div')], "kmeans", k=8, nstart = 25)
+p <- fviz_cluster(res.kmeans,
+                  main = 'Multivariate k-means Clustering (k=7)',
+                  repel = T,
+                  legend.title = 'Cluster') +
+  theme_minimal() +
+  guides(colour = F) +
+  theme(plot.title = element_text(face='bold'))
+p
